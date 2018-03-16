cbe51e943e5ef28adae08bca2da4bc9ea7928ef9
Manuel Silva <msilvacav@gmail.com>
2018-03-16 11:10:36 -0300

    nuevo

--- app/models/noticium.rb
+++ app/models/noticium.rb
@@ -1,4 +1,5 @@
 class Noticium < ApplicationRecord
   validates :bajada, presence: true, length: {maximum: 200}
+  validates :titulo, presence: true
   has_many :comments, :dependent => :delete_all
 end
